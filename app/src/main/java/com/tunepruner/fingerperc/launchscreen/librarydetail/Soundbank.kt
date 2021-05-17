94aa6e16cad31f8dffe8861b1d86f747fd81106b
Phil Carlson <philcarlson@Philips-MBP-2.cable.rcn.com>
2021-05-17 14:14:43 -0400

    Adds logic to Soundbank. About to change the database in Firebase...

--- app/src/main/java/com/tunepruner/fingerperc/launchscreen/librarydetail/Soundbank.kt
+++ app/src/main/java/com/tunepruner/fingerperc/launchscreen/librarydetail/Soundbank.kt
@@ -1,8 +1,36 @@
 package com.tunepruner.fingerperc.launchscreen.librarydetail
 
 
-class Soundbank(val libraries: ArrayList<Library>, val soundpacks: ArrayList<Soundpack>){
+class Soundbank(val libraries: ArrayList<Library>, val soundpacks: ArrayList<Soundpack>) {
+    private val functions = HashMap<CheckType, (library: Library) -> Boolean>()
 
+    init {
+        functions[CheckType.IS_RELEASED] = { getSoundpack(it)?.isReleased ?: false }
+        functions[CheckType.IS_PURCHASED] = { getSoundpack(it)?.isPurchased ?: false }
+        functions[CheckType.IS_INSTALLED] = { getSoundpack(it)?.isInstalled ?: false }
+    }
+
+    fun check(checkType: CheckType, library: Library): Boolean {
+        val result = functions[checkType]?.invoke(library)
+        return result ?: false
+    }
+
+    private fun getSoundpack(library: Library): Soundpack?{
+        for (soundpack in soundpacks) {
+            for (member in soundpack.members) {
+                if (member.soundpackID == library.soundpackID) {
+                    return soundpack
+                }
+            }
+        }
+        return null
+    }
+
+    enum class CheckType {
+        IS_RELEASED,
+        IS_PURCHASED,
+        IS_INSTALLED
+    }
 }
 
 data class Library(
@@ -34,6 +62,4 @@ data class Soundpack(
     @field:JvmField
     var isReleased: Boolean? = null,
     val members: ArrayList<Library> = ArrayList(),
-){
-
-}
+)
